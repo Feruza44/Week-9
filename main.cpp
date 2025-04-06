b2e211601887afbda5ad30d46801f89e4990b851
Feruza44 <f.bekmurotova@newuu.uz>
2025-04-06 21:40:21 +0500

    Hello World

--- main.cpp
+++ main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
 using namespace std;
-int main() {
-    cout << "HEllo World!" << endl;
-    return 0;
-}
+int main()
+// Problem 1
+//double calculateTotal(int item, int quantity) {
+  //  double price = 0;
+    //switch (item) {
+      //  case 1: price = 10; break;
+        //case 2: price = 8; break;
+      //  case 3: price = 5; break;
+      //  default: cout << "Invalid selection!\n"; return 0;
+    //}
+   // return price * quantity;}
+// Problem 2
+//{double calculateFee(int hours) {
+  //  if (hours <= 2) return 0;
+    //else if (hours <= 5) return (hours - 2) * 2;
+   // else return (3 * 2) + ((hours - 5) * 5);}
+// Problem 3:
+//double balance = 500;
+//void deposit(double amount) {
+ //   balance += amount;
+   // cout << "Deposited: $" << amount << ". New Balance: $" << balance << endl;}
+//void withdraw(double amount) {
+  //  if (amount > balance)
+    //    cout << "Insufficient funds!" << endl;
+  //  else {
+    //    balance -= amount;
+      //  cout << "Withdrawn: $" << amount << ". New Balance: $" << balance << endl;}}
+//void checkBalance() {
+ //   cout << "Current Balance: $" << balance << endl; }
+// Problem 4
+//double calculateCost(int movie, int tickets) {
+  //  double price;
+    //switch (movie) {
+      //  case 1: price = 8; break;
+      //  case 2: price = 10; break;
+      //  case 3: price = 12; break;
+      //  default: cout << "Invalid selection!\n"; return 0;}
+   // return price * tickets;}
+// Problem 5
+//double calculateFare(int train, int tickets) {
+  //  double price;
+    //switch (train) {
+      //  case 1: price = 15; break;
+        //case 2: price = 20; break;
+      //  case 3: price = 25; break;
+     //   default: cout << "Invalid train selection!\n"; return 0;}
+  //  return price * tickets;}
+// Problem 6
+//bool checkPassword(string password) {
+  //  bool hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
+    //for (char c : password) {
+      //  if (isupper(c)) hasUpper = true;
+        //else if (islower(c)) hasLower = true;
+       // else if (isdigit(c)) hasDigit = true;
+     //   else hasSpecial = true;}
+   // return (password.length() >= 8 && hasUpper && hasLower && hasDigit && hasSpecial);}
